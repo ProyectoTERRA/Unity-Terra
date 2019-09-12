2cee839c83cbfd8ba926a733bb821b4f8f133332
EmilioOrtega <pancrasio123456@gmail.com>
2019-09-11 21:54:02 -0500

    Agregar proyecto Unity

---
+++ TERRA/Assets/Codigos/getText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getText : MonoBehaviour
+{
+
+    public InputField nombre;
+    public Text setNombre;
+    public void setGet()
+    {
+        
+        setNombre.text ="Nombre: "+nombre.text;
+        Debug.Log(nombre+"        "+nombre.text);
+    }
+}
---
+++ TERRA/Assets/Codigos/newPartida.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class newPartida : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    } 
+
+    public void newpar(string boton)
+    {
+        SceneManager.LoadScene(boton);
+
+    }
+}
