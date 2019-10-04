06dc753c0d58a5fd893a0add579bdc77f9de2a5a
RezLex <omegaphyton@gmail.com>
2019-10-04 17:41:44 -0500

    Ahhh

---
+++ TERRA/Assets/Hability_1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hability_1 : MonoBehaviour
+{
+
+    public Sprite DoubleJump;
+    public Sprite LongJump;
+    public Sprite Invisible;
+    public Sprite Dash;
+
+    public Color g;
+
+    public SpriteRenderer spr;
+
+    public string set = "Double_Jump";
+
+    void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        spr.color = Color.white;
+        g = new Color(108.000f, 152.000f, 241.000f, 1.000f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(spr.color);
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log(spr.color);
+            spr.color = g;
+        }
+    }
+}
