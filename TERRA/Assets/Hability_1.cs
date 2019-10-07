c884264d43d1a9382350cd1b08d9f2679ba888ff
RezLex <omegaphyton@gmail.com>
2019-10-07 13:08:55 -0500

    Habilidad 1

--- TERRA/Assets/Hability_1.cs
+++ TERRA/Assets/Hability_1.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hability_1 : MonoBehaviour
 {
@@ -10,32 +11,101 @@ public class Hability_1 : MonoBehaviour
     public Sprite Invisible;
     public Sprite Dash;
 
-    public Color g;
+    public bool disp;
+    public bool act;
+
+    public float actD;
+    public float dispD;
+
+    public Color ac = new Color(0.8679245f, 0.3561766f, 0.3814742f, 1f);
+
 
     public SpriteRenderer spr;
 
-    public string set = "Double_Jump";
+    private string set = "Invisible";
 
     void Start()
     {
         spr = GetComponent<SpriteRenderer>();
-        spr.color = Color.white;
-        g = new Color(108.000f, 152.000f, 241.000f, 1.000f);
+
+        if (set == "Double_Jump")
+        {
+            spr.sprite = DoubleJump;
+            dispD = 120;
+            actD = 10;
+        }
+
+        else if (set == "Long_Jump")
+        {
+            spr.sprite = LongJump;
+            dispD = 120;
+            actD = 5;
+        }
+        else if (set == "Invisible")
+        {
+            spr.sprite = Invisible;
+            dispD = 240;
+            actD = 5;
+        }
+
+        else if (set == "Dash")
+        {
+            spr.sprite = Dash;
+            dispD = 20;
+            actD = 1;
+        }
+
+        disp = true;
+        act = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+
        
     }
 
     private void FixedUpdate()
     {
-        Debug.Log(spr.color);
-        if (Input.GetKeyDown(KeyCode.B))
+        if (disp)
         {
-            spr.color = g;
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                act = true;
+                disp = false;
+                Invoke("Activate",actD);            
+            }
+            spr.color = Color.white;
+        }
+        else if(act)
+        {
+            
+
+            spr.color = ac;
             Debug.Log(spr.color);
         }
+        else if (!act && !disp)
+        {
+            spr.color = Color.gray;
+        }
+
+
+    }
+
+
+    public void Activate()
+    {
+        act = false;
+        disp = false;
+        Invoke("Dispo", dispD);
+
+    }
+
+    public void Dispo()
+    {
+        act = false;
+        disp = true;
+
     }
 }
