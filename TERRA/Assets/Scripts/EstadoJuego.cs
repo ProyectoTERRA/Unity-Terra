d00e4b4e9742e1ec78189a3d8f55e316efc57686
EmilioOrtega <pancrasio123456@gmail.com>
2019-10-10 00:33:23 -0500

    Guardar archivo con una variable y cargar archivo

--- TERRA/Assets/Scripts/EstadoJuego.cs
+++ TERRA/Assets/Scripts/EstadoJuego.cs
@@ -9,27 +9,7 @@ using UnityEngine.UI;
 
 public class EstadoJuego : MonoBehaviour
 {
-    public InputField nombre;
-    public Text setNombre;
-    public string nombrePartida="", nuevoNombre="";
-    private string rutaArchivo;
 
-    public static EstadoJuego estadoJuego;
-
-
-    void Awake()
-    {
-        rutaArchivo = Application.persistentDataPath + "/datos.dat";
-        if(estadoJuego==null)
-        {
-            estadoJuego = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else if(estadoJuego != this)
-        {
-            Destroy(gameObject);
-        }
-    }
     // Start is called before the first frame update
     void Start()
     {
@@ -39,61 +19,12 @@ public class EstadoJuego : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
 
-    public void guardar()
-    {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(rutaArchivo);
-
-        getName();
-
-        DatosGuardados datos = new DatosGuardados();
-        datos.nombre = nombrePartida;
-
-        bf.Serialize(file, nombrePartida);
-
-        file.Close();
     }
 
     public void hacer()
     {
-        guardar();
-        Cargar();
-    }
-    public void getName()
-    {
-        nombrePartida = nombre.text;
-    }
 
-    void Cargar()
-    {
-        if(File.Exists(rutaArchivo))
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(rutaArchivo, FileMode.Open);
+    }   
 
-            DatosGuardados datos = (DatosGuardados)bf.Deserialize(file);
-
-            nuevoNombre = datos.nombre;
-
-            setNombre.text = nuevoNombre;
-
-            file.Close();
-        }
-        else
-        {
-            setNombre.text = "No funciono";
-        }
-        
-    }
-
-    
-
-}
-[Serializable]
-public class DatosGuardados
-{
-    public String nombre;
 }
---
+++ TERRA/Assets/Scripts/SistemaGuardado.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class SistemaGuardado : MonoBehaviour
+{
+
+    public String nombrePartida = "";
+    public String buscarNombre = "", prueba;
+    bool cargarInicio;
+    public int basura;
+    bool guardaAutomatico;
+    // Start is called before the first frame update
+    void Start()
+    {
+        guardaAutomatico = Input.GetKeyDown(KeyCode.Space);
+        Debug.Log(Application.persistentDataPath);
+        Debug.Log("Hola");
+        if(cargarInicio)
+        {
+            cargar();
+        }
+        if (guardaAutomatico)
+        {
+            guardar();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetButton("btnAceptar"))
+        {
+
+        }*/
+    }
+
+    public void guardar()
+    {
+        BinaryFormatter bf = new BinaryFormatter(); //Ayudante
+        FileStream expediente = File.Create(Application.persistentDataPath + "/" + nombrePartida + ".d");//Crea archivo datos.d
+        DatosJuego dato = new DatosJuego(); //Daro es la variable de la clase que se serializa
+
+        //Variables a guardar
+        dato.nombrePartida = nombrePartida;
+        dato.basura = 4;
+        //Serializara los arrchivos
+        bf.Serialize(expediente, dato);
+        expediente.Close();
+
+        Debug.Log("COOL");
+    }
+
+    public void cargar()
+    {
+        if(File.Exists(Application.persistentDataPath+"/"+buscarNombre+".d"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream expediente = File.OpenRead(Application.persistentDataPath + "/" + buscarNombre + ".d");
+            DatosJuego datos = new DatosJuego();
+
+            datos = bf.Deserialize(expediente) as DatosJuego;
+
+            prueba = datos.nombrePartida;
+            basura = datos.basura;
+
+            Debug.Log("COOL");
+        }
+        else { Debug.Log("No se encontro el archivo"); }
+    }
+
+    public void get(InputField name)
+    {
+        nombrePartida = name.text;
+        guardar();
+    }
+
+}
+
+[Serializable()]//Datos listos para serializar
+
+class DatosJuego:System.Object
+{
+    public String nombrePartida;
+    public int basura;
+}
