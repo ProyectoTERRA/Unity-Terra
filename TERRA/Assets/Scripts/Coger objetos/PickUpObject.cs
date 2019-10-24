6779e34e19fec4a5ff6c5f5e81ef52a69bcaf9ca
EmilioOrtega <pancrasio123456@gmail.com>
2019-10-24 00:17:33 -0500

    Recoger y soltar objeto

---
+++ TERRA/Assets/Scripts/Coger objetos/PickUpObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    public GameObject objectToPick, pickedObject;
+    public Transform interactionZone;
+
+    void Update()
+    {
+        if(objectToPick != null && objectToPick.GetComponent<Pickable>().coger == true && pickedObject == null)
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                pickedObject = objectToPick;
+                pickedObject.GetComponent<Pickable>().coger = false;
+                pickedObject.transform.SetParent(interactionZone);
+                pickedObject.transform.position = interactionZone.position;
+                pickedObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+                pickedObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            }
+        }
+        else if(pickedObject!=null)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                pickedObject.GetComponent<Pickable>().coger = true;
+                pickedObject.transform.SetParent(null);
+                pickedObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+                pickedObject.GetComponent<Rigidbody2D>().isKinematic = true;
+                pickedObject = null;
+            }
+        }
+    }
+}
---
+++ TERRA/Assets/Scripts/Coger objetos/Pickable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickable : MonoBehaviour
+{
+    public bool coger = true;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "PlayerInteractionZone")
+        {
+            collision.GetComponentInParent<PickUpObject>().objectToPick = this.gameObject;
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "PlayerInteractionZone")
+        {
+            collision.GetComponentInParent<PickUpObject>().objectToPick = null;
+
+        }
+    }
+}
