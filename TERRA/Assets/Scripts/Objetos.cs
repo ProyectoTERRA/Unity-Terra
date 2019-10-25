dae2cad2bb069d805c2fc23be49a9168ccde4b08
RezLex <omegaphyton@gmail.com>
2019-10-25 10:58:36 -0500

    Frefab Manzana

---
+++ TERRA/Assets/Scripts/Objetos.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objetos : MonoBehaviour
+{
+    
+    public string manzana;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject go = GameObject.Find("InvFunc");
+        radial radial = go.GetComponent<radial>();
+        
+        
+        Debug.Log("Hay una colision");
+        if(other.gameObject.tag =="manzana")
+        {
+            manzana = other.gameObject.name;
+            Debug.Log("Ha habido una colision con la manzana");
+            radial.basura[3]++;
+
+            Destroy(GameObject.Find(manzana));
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
+
