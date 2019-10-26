2f4fdcefbfa99fa3f10b087aaba55b43437cbdfe
EmilioOrtega <pancrasio123456@gmail.com>
2019-10-25 19:28:22 -0500

    Esta el panel para agregar a inventario

--- TERRA/Assets/Scripts/Objetos.cs
+++ TERRA/Assets/Scripts/Objetos.cs
@@ -4,26 +4,14 @@ using UnityEngine;
 
 public class Objetos : MonoBehaviour
 {
-    
     public string manzana;
-    
-
-
-    // Start is called before the first frame update
     void Start()
     {
 
-        
-
-
-
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         
     }
 
@@ -31,21 +19,13 @@ public class Objetos : MonoBehaviour
     {
         GameObject go = GameObject.Find("InvFunc");
         radial radial = go.GetComponent<radial>();
-        
-        
-        Debug.Log("Hay una colision");
-        if(other.gameObject.tag =="manzana")
-        {
-            manzana = other.gameObject.name;
-            Debug.Log("Ha habido una colision con la manzana");
-            radial.basura[3]++;
+        manzana = other.gameObject.name;
 
+        if (manzana =="manzana")
+        {
+            radial.basura[3]++;
             Destroy(GameObject.Find(manzana));
         }
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        
     }
 }
 
