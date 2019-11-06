4eb246d391334f2b89e8641332d6ae985586d67d
RezLex <omegaphyton@gmail.com>
2019-11-05 18:02:22 -0600

    update Casa

--- TERRA/Assets/Scripts/PlayerCasa.cs
+++ TERRA/Assets/Scripts/PlayerCasa.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class PlayerCasa : MonoBehaviour
 {
     [SerializeField] private GameObject camera;
+
+    public bool lucy;
+
     // Start is called before the first frame update
     void Start()
     {
         camera.transform.position = new Vector3(0.0f, 0.0f, - 10.0f);
         camera.transform.localScale = new Vector3(1f, 1f, 1f);
+
+        transform.position = new Vector3(-6.4f, 0.5f);
+        transform.localScale = new Vector3(1.75f, 1.75f, 1.75f);
+
+        lucy = false;
+
     }
 
     // Update is called once per frame
@@ -33,7 +42,31 @@ public class PlayerCasa : MonoBehaviour
                 camera.transform.localScale = new Vector3(1f, 1f, 1f);
             }
         }
-        
+
+        if (collision.gameObject.tag == "PuertaSala")//compara si hizo la colision con el objeto correcto
+        {
+            Debug.Log("Has tocado la puerta3");
+            if (Input.GetKeyDown(KeyCode.E) && lucy)
+            {
+                Debug.Log("Tecla e");
+                transform.position = new Vector3(30.6f, 0.5f);
+                transform.localScale = new Vector3(1.75f, 1.75f, 1.75f);
+
+                camera.transform.position = new Vector3(36.0f, 0.0f, -10.0f);
+                camera.transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+        }
+
+        if (collision.gameObject.tag == "Lucy")//compara si hizo la colision con el objeto correcto
+        {
+            Debug.Log("Has tocado a Lucy");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Tecla e");
+                lucy = true;
+            }
+        }
+
 
     }
 }
