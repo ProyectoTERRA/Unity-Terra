cef864bacda28b8bd7ed282591b8770500427a9a
RezLex <omegaphyton@gmail.com>
2019-12-13 08:49:25 -0600

    esferas 0.2v

--- TERRA/Assets/Scripts/Esferas.cs
+++ TERRA/Assets/Scripts/Esferas.cs
@@ -6,28 +6,28 @@ public class Esferas : MonoBehaviour
 {
 
     private Rigidbody2D rbd2;
-    public float JumpPower = 6.5f;
-    private int side;
+    public float JumpPower = 1.0f;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-     
+        this.GetComponent<Rigidbody2D>().velocity = new Vector3(8f*(-PlayerController.side), 2f);
 
 
         rbd2 = GetComponent<Rigidbody2D>();
         transform.position = new Vector3(transform.position.x - (1.5f * PlayerController.side), transform.position.y + 0.6f);
 
-        rbd2.AddForce(((Vector2.left * PlayerController.side)+ Vector2.up)  * JumpPower, ForceMode2D.Impulse);
+        rbd2.AddForce(Vector2.left * PlayerController.side * JumpPower, ForceMode2D.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-
-
-        
+        Destroy(gameObject,1f);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
     }
 }
