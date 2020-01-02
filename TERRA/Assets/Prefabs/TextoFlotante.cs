21ef2244826e78098da93732dc96f0afe6ec52c7
TGmer <55259180+TGmer@users.noreply.github.com>
2020-01-02 14:48:27 -0600

    Aún más Capitulo 2

---
+++ TERRA/Assets/Prefabs/TextoFlotante.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TextoFlotante : MonoBehaviour
+{
+    public float TiempoVida = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TiempoVida -= Time.deltaTime;
+        if (TiempoVida <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ TERRA/Assets/Scripts/Capitulo 2/Dialogue_Manager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue_Manager : MonoBehaviour
+{
+    public Dialogue dialogue;
+    Queue<string> sentences;
+    public GameObject DialoguePanel;
+    string activeSentence;
+    public float typingSpeed;
+    public TextMeshProUGUI displayText;
+    AudioSource myAudio;
+    public AudioClip speakSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        myAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void StartDialogue()
+    {
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentenceList)
+        {
+            
+            //toma la oración y la agrega a la cola, para despues ser presentada
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    void DisplayNextSentence()
+    {
+        if(sentences.Count <= 0)
+        {
+            displayText.text = activeSentence;
+            return;
+        }
+
+        activeSentence = sentences.Dequeue();
+        displayText.text = activeSentence;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        if (col.CompareTag("Player"))
+        {
+            DialoguePanel.SetActive(true);
+            StartDialogue();
+            Debug.Log("Colision Inicial");
+        }  
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+       
+           if (Input.GetKeyDown(KeyCode.E) & other.CompareTag("Player"))
+                {
+                    DisplayNextSentence();
+                    Debug.Log("Colision Jugador");
+                }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D salida)
+    {
+        if (salida.CompareTag("Player"))
+        {
+            DialoguePanel.SetActive(false);
+            Debug.Log("Adios");
+        }
+    }
+}
