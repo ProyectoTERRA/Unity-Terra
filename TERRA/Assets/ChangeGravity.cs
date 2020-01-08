6ccbfb9dfbf6c797adb4405517c9c071392808b1
TGmer <55259180+TGmer@users.noreply.github.com>
2020-01-08 12:42:31 -0600

    CAP 6 EL REMIX

---
+++ TERRA/Assets/ChangeGravity.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGravity : MonoBehaviour
+{
+    Rigidbody2D myRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = this.GetComponent<Rigidbody2D>();
+    }
+
+    void AddForceUP()
+    {
+        myRB.velocity = new Vector2(0, 0);
+        myRB.AddForce(new Vector2(0, 7), ForceMode2D.Impulse);
+    }
+
+    void AddForceDown()
+    {
+        myRB.velocity = new Vector2(0, 0);
+        myRB.AddForce(new Vector2(0, -7), ForceMode2D.Impulse);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "GArriba") //&& Input.GetKey(KeyCode.E)
+        {
+            Debug.Log("Funciona");
+            Physics2D.gravity = new Vector3(0, -1, 0);
+            AddForceUP();
+        }
+
+        if (collision.name == "GAbajo") //&& Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("Funciona");
+            Physics2D.gravity = new Vector3(0, 1, 0);
+            AddForceDown();
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
