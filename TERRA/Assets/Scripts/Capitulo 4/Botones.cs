8a5bba1900762de219ffccea3552f61beea8f9f4
EmilioOrtega <pancrasio123456@gmail.com>
2020-01-15 15:55:41 -0600

    cap4

---
+++ TERRA/Assets/Scripts/Capitulo 4/Botones.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Botones : MonoBehaviour
+{
+    string nombre = "";
+    public GameObject cartel, boton1, boton2, boton3, boton4, boton5, bt1, bt2, bt3, bt4, bt5;
+    // Start is called before the first frame update
+    private void Update()
+    {
+        if (nombre.Length >= 5)
+        {
+            
+            if (nombre == "reusa")
+            {
+                Debug.Log("Nombre correcto " + nombre);
+                cartel.SetActive(true);
+                StartCoroutine(apagarcartel());
+                
+            }
+            else
+            {
+                Debug.Log("Nombre incorrecto " + nombre);
+                nombre = "";
+                boton1.SetActive(false);
+                boton2.SetActive(false);
+                boton3.SetActive(false);
+                boton4.SetActive(false);
+                boton5.SetActive(false);
+                bt1.SetActive(true);
+                bt2.SetActive(true);
+                bt3.SetActive(true);
+                bt4.SetActive(true);
+                bt5.SetActive(true);
+            }
+        }
+    }
+    IEnumerator apagarcartel()
+    {
+        yield return new WaitForSeconds(5);
+        cartel.SetActive(false);
+        nombre = "";
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "coco")
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                string letra = collision.gameObject.name;
+                if (letra == "botonApagado")
+                {
+                    nombre += "e";
+                    boton1.SetActive(true);
+                    bt1.SetActive(false);
+                }
+                if (letra == "botonApagado1")
+                {
+                    nombre += "r";
+                    boton2.SetActive(true);
+                    bt2.SetActive(false);
+                }
+                if (letra == "botonApagado2")
+                {
+                    nombre += "u";
+                    boton3.SetActive(true);
+                    bt3.SetActive(false);
+                }
+                if (letra == "botonApagado3")
+                {
+                    nombre += "a";
+                    boton4.SetActive(true);
+                    bt4.SetActive(false);
+                }
+                if (letra == "botonApagado4")
+                {
+                    nombre += "s";
+                    boton5.SetActive(true);
+                    bt5.SetActive(false);
+                }
+                Debug.Log("Nombre " + nombre);
+            }
+
+        }
+    }
+}
--- TERRA/Assets/Scripts/Dialogos/ENEMIGOS/Watcher.cs
+++ TERRA/Assets/Scripts/Dialogos/ENEMIGOS/Watcher.cs
@@ -31,18 +31,7 @@ public class Watcher : MonoBehaviour
 
         if (col.gameObject.tag == "Player")
         {
-            col.SendMessage("EnemyKnockBack", transform.position.x+1);/*
-            Debug.Log("Ha hecho colision con el jugador");
-            float yOffset = 0.04f;
-            if (transform.position.y + yOffset < col.transform.position.y)
-            {
-                col.SendMessage("EnemyJump");
-                Destroy(gameObject);
-            }
-            else
-            {
-                col.SendMessage("EnemyKnockBack", transform.position.x);
-            }*/
+            col.SendMessage("EnemyKnockBack", transform.position.x+1);
 
         }
     }
@@ -56,48 +45,13 @@ public class Watcher : MonoBehaviour
         //Si la distancia al jugador es menor que el radio de vision del objetivo, el target pasa a ser el jugador
         float distanciaJugador = Vector3.Distance(Jugador.transform.position, transform.position);
         if (distanciaJugador < DistanciaVision) target = Jugador.transform.position;
-        /*
-        {
-            target = Jugador.transform.position;
-            if(ColorUtility.TryParseHtmlString("#C2C2C2",out color))
-            {
-                matchPanel.GetComponent<Watcher>().color = color;
-            }
-            
-        }
-        */
+        
         //Movimiento del Enemigo
         float fixedSpeed = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
         Debug.DrawLine(transform.position, target, Color.red);
 
-
-        /*
-        if(transform.position.x < 0)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        }else if(transform.position.x > 0)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
-    
-        /*
-        if (rbd2.velocity.x > -0.01f && rbd2.velocity.x < 0.01f)
-        {
-            speed = -speed;
-            rbd2.velocity = new Vector2(speed, rbd2.velocity.y);
-        }
-
-        if (speed < 0)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-
-        }
-        else if (speed > 0)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
-   */
+     
 
     }
 }
