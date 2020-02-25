27ce7693bc988e3bdff08546c70ef2b1ce4232f5
EmilioOrtega <pancrasio123456@gmail.com>
2020-02-24 18:09:18 -0600

    Casi todo terminado

--- TERRA/Assets/Scripts/oracion.cs
+++ TERRA/Assets/Scripts/oracion.cs
@@ -6,7 +6,7 @@ public class oracion : MonoBehaviour
     public static string frase;
     public static int contador;
     public GameObject hoja, mensaje, mensaje1;
-
+    int bandera = 0;
     void Update()
     {
         if (contador == 15)
@@ -14,8 +14,11 @@ public class oracion : MonoBehaviour
             Debug.Log(frase + " " + contador);
             if (frase == "bbcddcbaggabbaa")
             {
-                hoja.SetActive(true);
-                Debug.Log("Logrado");
+                if (bandera == 0)
+                {
+                    bandera++;
+                    hoja.SetActive(true);
+                }
             }
             else
             {
