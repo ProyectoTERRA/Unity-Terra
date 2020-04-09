672ed82db1fe8e738367de3c25fb9b86ae8cb69c
RezLex <omegaphyton@gmail.com>
2020-04-09 03:57:16 -0500

    Conductos v1.2

---
+++ TERRA/Assets/Scripts/Capitulo 5/PlayerConductos.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConductos : MonoBehaviour
+{
+    [SerializeField] private GameObject camera;
+    private bool Sec2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sec2 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Tran")//compara si hizo la colision con el objeto correcto
+        {
+            
+            camera.transform.position = new Vector3(15f, -8f, -10.0f);
+            Sec2 = true;
+
+        }
+    }
+}
---
+++ TERRA/Assets/Scripts/Capitulo 5/Trampillas.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampillas : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private SpriteRenderer OnTrap;
+    private BoxCollider2D Bye;
+    private bool Active;
+    void Start()
+    {
+        Active = false;
+        OnTrap = GetComponent<SpriteRenderer>();
+        Bye = GetComponent<BoxCollider2D>();
+        StartCoroutine(Trap());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Active = true;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Active = false;
+        }
+    }
+    IEnumerator Trap()
+    {
+        float rWait = Random.Range(1f, 5f);
+
+        yield return new WaitForSeconds(rWait);
+        OnTrap.enabled = false;
+        Bye.enabled = false;
+        rWait = Random.Range(1f, 5f);
+        yield return new WaitForSeconds(1f);
+        if (!Active)
+        {
+            OnTrap.enabled = true;
+            Bye.enabled = true;
+
+        }
+        
+        StartCoroutine(Trap());
+
+    }
+}
