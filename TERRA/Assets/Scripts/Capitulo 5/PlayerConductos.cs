6c3631cac4a988c7f10642211bc11a5d65181fc9
RezLex <omegaphyton@gmail.com>
2020-04-10 15:18:54 -0500

    Conductos v3.0

--- TERRA/Assets/Scripts/Capitulo 5/PlayerConductos.cs
+++ TERRA/Assets/Scripts/Capitulo 5/PlayerConductos.cs
@@ -1,21 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerConductos : MonoBehaviour
 {
     [SerializeField] private GameObject camera;
+    [SerializeField] private GameObject Slider;
+    [SerializeField] private GameObject TramplillaF;
+    [SerializeField] private GameObject list;
+    public Slider ActS;
     private bool Sec2;
+    bool flag;
 
     // Start is called before the first frame update
     void Start()
     {
+        flag = true;
+ 
+        GetComponent<SliderConductos>().enabled = false;
+        ActS.enabled = false;
+        Slider.SetActive(false);
         Sec2 = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (SliderConductos.Target)
+        {
+            Destroy(TramplillaF);
+            GetComponent<PlayerController>().enabled = true ;
+
+            if (flag)
+            {
+                GameObject go = GameObject.Find("InvFunc");
+                radial radial = go.GetComponent<radial>();
+                radial.especiales[2]--;
+                string normal = "ganzua";
+                if (radial.especiales[2] <= 0) list.SendMessage("remove", normal);
+                flag = false;
+            }
+
+        }
 
 
     }
@@ -26,6 +53,17 @@ public class PlayerConductos : MonoBehaviour
             
             camera.transform.position = new Vector3(15f, -8f, -10.0f);
             Sec2 = true;
+
+        }
+    }
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Accionador" && Input.GetKeyDown(KeyCode.J) && PlayerController.Equip == "Especiales_2")//compara si hizo la colision con el objeto correcto
+        {
+            GetComponent<SliderConductos>().enabled = true;
+            GetComponent<PlayerController>().enabled = false;
+            ActS.enabled = true;
+            Slider.SetActive(true);
 
         }
     }
---
+++ TERRA/Assets/Scripts/Capitulo 5/SliderConductos.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SliderConductos : MonoBehaviour
+{
+    public static bool Target, active;
+    private bool dir;
+    [SerializeField] private GameObject Cavas;
+    public Slider Bar;
+
+    void Start()
+    {
+        dir = true;
+        Target = false;
+        active = true;
+        Bar.value = 0;
+        StartCoroutine(push());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            active = false;
+            if(Bar.value >= 20 && Bar.value <= 26)
+            {
+                StartCoroutine(win());
+            }
+            else
+            {
+                StartCoroutine(reset());
+            }
+            
+        }
+        if (Bar.value == 0 && !Target)
+        {
+            dir = true;
+        }
+        else if (Bar.value == 50 && !Target)
+        {
+            dir = false;
+        }
+
+    }
+    public IEnumerator reset()
+    {
+        yield return new WaitForSeconds(3f);
+        active = true;
+        StartCoroutine(push());
+    }
+    public IEnumerator win()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Target = true;
+        Cavas.SetActive(false);
+        Bar.enabled = false;
+
+    }
+    public IEnumerator push()
+    {
+        if (dir)
+        {
+            Bar.value = Bar.value + 2;
+        }
+        else
+        {
+            Bar.value = Bar.value - 2;
+        }
+        yield return new WaitForSeconds(0.01f);
+
+       
+
+        if (active)
+        {
+            StartCoroutine(push());
+        }
+
+
+    }
+}
