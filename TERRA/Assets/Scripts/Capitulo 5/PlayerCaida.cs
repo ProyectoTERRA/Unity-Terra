bf536637250d706be8dadf67cb3c1a8bd3499a26
RezLex <omegaphyton@gmail.com>
2020-04-15 15:13:44 -0500

    Caída v1.0

--- TERRA/Assets/Scripts/Capitulo 5/PlayerCaida.cs
+++ TERRA/Assets/Scripts/Capitulo 5/PlayerCaida.cs
@@ -4,21 +4,65 @@ using UnityEngine;
 
 public class PlayerCaida : MonoBehaviour
 {
+    [SerializeField] private GameObject camera;
     public float speed = 5f;
+    private bool move;
     // Start is called before the first frame update
     void Start()
     {
-        
+        move = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hInput = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(hInput*speed * Time.deltaTime, 0, 0);
-        float newx = Mathf.Clamp(transform.position.x, -12.5f, 5f);
-        transform.position = new Vector3(newx, transform.position.y, 0);
+        if (move)
+        {
+            float hInput = Input.GetAxis("Horizontal");
+            transform.position += new Vector3(hInput * speed * Time.deltaTime, 0, 0);
+            float newx = Mathf.Clamp(transform.position.x, -12.5f, 5f);
+            transform.position = new Vector3(newx, transform.position.y, 0);
+        }
+        
+
+        if (camera.transform.position.y <= -23.8)
+        {
+            camera.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+
+        }
+
+    }
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Shit")
+        {
+            Debug.Log("Uff");
+            ShitKnockBack();
+        }
+        if (collision.gameObject.name == "FINISH")
+        {
+            Debug.Log("Adioooooos");
+        }
+    }
+
+    public void ShitKnockBack()
+    {
+
+        move = false;
 
 
+        Invoke("EnableMovement", 2f);
+
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+    void EnableMovement()
+    {
+        move = true;
+        GetComponent<SpriteRenderer>().color = Color.white;
     }
 }
---
+++ TERRA/Assets/Scripts/Capitulo 5/ShitActivator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShitActivator : MonoBehaviour
+{
+    [SerializeField] private GameObject Parte1;
+    [SerializeField] private GameObject Parte2;
+    [SerializeField] private GameObject Parte3;
+    [SerializeField] private GameObject Parte4;
+    [SerializeField] private GameObject Parte5;
+    // Start is called before the first frame update
+    private float rmX, rmR;
+    private int rmM;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Action")
+        {
+            Debug.Log("Puto");
+            rmM = Random.Range(1, 5);
+            if (rmM == 1)
+            {
+                rmR = Random.Range(0f, 359f);
+                rmX = Random.Range(-12.5f, 5f);
+                Instantiate(Parte1, new Vector3(rmX, transform.position.y - 5f, 0f), Quaternion.Euler(0f, 0f, rmR));
+            }
+            else if (rmM == 2)
+            {
+                rmR = Random.Range(0f, 359f);
+                rmX = Random.Range(-12.5f, 5f);
+                Instantiate(Parte2, new Vector3(rmX, transform.position.y - 5f, 0f), Quaternion.Euler(0f, 0f, rmR));
+            }
+            else if (rmM == 3)
+            {
+                rmR = Random.Range(0f, 359f);
+                rmX = Random.Range(-12.5f, 5f);
+                Instantiate(Parte3, new Vector3(rmX, transform.position.y - 5f, 0f), Quaternion.Euler(0f, 0f, rmR));
+            }
+            else if (rmM == 4)
+            {
+                rmR = Random.Range(0f, 359f);
+                rmX = Random.Range(-12.5f, 5f);
+                Instantiate(Parte4, new Vector3(rmX, transform.position.y - 5f, 0f), Quaternion.Euler(0f, 0f, rmR));
+            }
+            else
+            {
+                rmR = Random.Range(0f, 359f);
+                rmX = Random.Range(-12.5f, 5f);
+                Instantiate(Parte5, new Vector3(rmX, transform.position.y - 5f, 0f), Quaternion.Euler(0f, 0f, rmR));
+            }
+        }
+    }
+}
