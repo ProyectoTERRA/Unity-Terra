171f69fdc0c7216fd435f86c30668f694aa02772
RezLex <omegaphyton@gmail.com>
2020-04-28 21:21:32 -0500

    PalTito

--- TERRA/Assets/MinigameManager.cs
+++ TERRA/Assets/MinigameManager.cs
@@ -17,7 +17,6 @@ public class MinigameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        /*
         #region RANDOM
         N1 = Random.Range(0, 10);
         N2 = Random.Range(0, 10);
@@ -29,7 +28,372 @@ public class MinigameManager : MonoBehaviour
         C3 = Random.Range(0, 10);
         C4 = Random.Range(0, 10);
         #endregion
-        */
+        
+
+        Debug.Log(N1);
+        Debug.Log(N2);
+        Debug.Log(N3);
+        Debug.Log(N4);
+
+        Debug.Log(C1);
+        Debug.Log(C2);
+        Debug.Log(C3);
+        Debug.Log(C4);
+
+
+        #region Colores
+
+        //----------- C1 -----------------
+
+        if (C1 == 0)
+        {
+            col1.color = Color.black;
+        }
+        else if (C1 == 1)
+        {
+            col1.color = Cafe;
+        }
+        else if (C1 == 2)
+        {
+            col1.color = Color.red;
+        }
+        else if (C1 == 3)
+        {
+            col1.color = Naranja;
+        }
+        else if (C1 == 4)
+        {
+            col1.color = Color.yellow;
+        }
+        else if (C1 == 5)
+        {
+            col1.color = Color.green;
+        }
+        else if (C1 == 6)
+        {
+            col1.color = Color.blue;
+        }
+        else if (C1 == 7)
+        {
+            col1.color = Morado;
+        }
+        else if (C1 == 8)
+        {
+            col1.color = Color.gray;
+        }
+        else if (C1 == 9)
+        {
+            col1.color = Color.white;
+        }
+
+        //----------- C2 ----------------
+
+        if (C2 == 0)
+        {
+            col2.color = Color.black;
+        }
+        else if (C2 == 1)
+        {
+            col2.color = Cafe;
+        }
+        else if (C2 == 2)
+        {
+            col2.color = Color.red;
+        }
+        else if (C2 == 3)
+        {
+            col2.color = Naranja;
+        }
+        else if (C2 == 4)
+        {
+            col2.color = Color.yellow;
+        }
+        else if (C2 == 5)
+        {
+            col2.color = Color.green;
+        }
+        else if (C2 == 6)
+        {
+            col2.color = Color.blue;
+        }
+        else if (C2 == 7)
+        {
+            col2.color = Morado;
+        }
+        else if (C2 == 8)
+        {
+            col2.color = Color.gray;
+        }
+        else if (C2 == 9)
+        {
+            col2.color = Color.white;
+        }
+
+        //----------- C3 ----------------
+
+        if (C3 == 0)
+        {
+            col3.color = Color.black;
+        }
+        else if (C3 == 1)
+        {
+            col3.color = Cafe;
+        }
+        else if (C3 == 2)
+        {
+            col3.color = Color.red;
+        }
+        else if (C3 == 3)
+        {
+            col3.color = Naranja;
+        }
+        else if (C3 == 4)
+        {
+            col3.color = Color.yellow;
+        }
+        else if (C3 == 5)
+        {
+            col3.color = Color.green;
+        }
+        else if (C3 == 6)
+        {
+            col3.color = Color.blue;
+        }
+        else if (C3 == 7)
+        {
+            col3.color = Morado;
+        }
+        else if (C3 == 8)
+        {
+            col3.color = Color.gray;
+        }
+        else if (C3 == 9)
+        {
+            col3.color = Color.white;
+        }
+
+        //----------- C4 ----------------
+
+        if (C4 == 0)
+        {
+            col4.color = Color.black;
+        }
+        else if (C4 == 1)
+        {
+            col4.color = Cafe;
+        }
+        else if (C4 == 2)
+        {
+            col4.color = Color.red;
+        }
+        else if (C4 == 3)
+        {
+            col4.color = Naranja;
+        }
+        else if (C4 == 4)
+        {
+            col4.color = Color.yellow;
+        }
+        else if (C4 == 5)
+        {
+            col4.color = Color.green;
+        }
+        else if (C4 == 6)
+        {
+            col4.color = Color.blue;
+        }
+        else if (C4 == 7)
+        {
+            col4.color = Morado;
+        }
+        else if (C4 == 8)
+        {
+            col4.color = Color.gray;
+        }
+        else if (C4 == 9)
+        {
+            col4.color = Color.white;
+        }
+
+        #endregion
+
+        #region Numeros
+
+        //----------- N1 -----------------
+
+        if (N1 == 0)
+        {
+            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S0;
+        }
+        else if (N1 == 1)
+        {
+            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S1;
+        }
+        else if (N1 == 2)
+        {
+            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S2;
+        }
+        else if (N1 == 3)
+        {
+            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S3;
+        }
+        else if (N1 == 4)
+        {
+            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S4;
+        }
+        else if (N1 == 5)
+        {
+            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S5;
+        }
+        else if (N1 == 6)
+        {
+            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S6;
+        }
+        else if (N1 == 7)
+        {
+            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S7;
+        }
+        else if (N1 == 8)
+        {
+            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S8;
+        }
+        else if (N1 == 9)
+        {
+            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S9;
+        }
+
+        //----------- N2 -----------------
+
+        if (N2 == 0)
+        {
+            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S0;
+        }
+        else if (N2 == 1)
+        {
+            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S1;
+        }
+        else if (N2 == 2)
+        {
+            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S2;
+        }
+        else if (N2 == 3)
+        {
+            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S3;
+        }
+        else if (N2 == 4)
+        {
+            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S4;
+        }
+        else if (N2 == 5)
+        {
+            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S5;
+        }
+        else if (N2 == 6)
+        {
+            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S6;
+        }
+        else if (N2 == 7)
+        {
+            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S7;
+        }
+        else if (N2 == 8)
+        {
+            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S8;
+        }
+        else if (N2 == 9)
+        {
+            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S9;
+        }
+
+        //----------- N3 -----------------
+
+        if (N3 == 0)
+        {
+            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S0;
+        }
+        else if (N3 == 1)
+        {
+            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S1;
+        }
+        else if (N3 == 2)
+        {
+            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S2;
+        }
+        else if (N3 == 3)
+        {
+            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S3;
+        }
+        else if (N3 == 4)
+        {
+            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S4;
+        }
+        else if (N3 == 5)
+        {
+            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S5;
+        }
+        else if (N3 == 6)
+        {
+            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S6;
+        }
+        else if (N3 == 7)
+        {
+            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S7;
+        }
+        else if (N3 == 8)
+        {
+            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S8;
+        }
+        else if (N3 == 9)
+        {
+            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S9;
+        }
+
+        //----------- N4 -----------------
+
+        if (N4 == 0)
+        {
+            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S0;
+        }
+        else if (N4 == 1)
+        {
+            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S1;
+        }
+        else if (N4 == 2)
+        {
+            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S2;
+        }
+        else if (N4 == 3)
+        {
+            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S3;
+        }
+        else if (N4 == 4)
+        {
+            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S4;
+        }
+        else if (N4 == 5)
+        {
+            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S5;
+        }
+        else if (N4 == 6)
+        {
+            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S6;
+        }
+        else if (N4 == 7)
+        {
+            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S7;
+        }
+        else if (N4 == 8)
+        {
+            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S8;
+        }
+        else if (N4 == 9)
+        {
+            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S9;
+        }
+
+        #endregion
+
+
     }
 
     // Update is called once per frame
@@ -38,7 +402,7 @@ public class MinigameManager : MonoBehaviour
 
 
     void Update()
-    {
+    {/* 
         N1 = Random.Range(0, 10);
         N2 = Random.Range(0, 10);
         N3 = Random.Range(0, 10);
@@ -48,133 +412,6 @@ public class MinigameManager : MonoBehaviour
         C2 = Random.Range(0, 10);
         C3 = Random.Range(0, 10);
         C4 = Random.Range(0, 10);
-
-        #region Colores
-        if (C1 == 0 || C2 == 0 || C3 == 0 || C4 == 0)
-        {
-            col1.color = Color.black;
-        }
-        if (C1 == 1 || C2 == 1 || C3 == 1 || C4 == 1)
-        {
-            col1.color = Cafe;
-        }
-        if (C1 == 2 || C2 == 2 || C3 == 2 || C4 == 2)
-        {
-            col1.color = Color.red;
-        }
-        if (C1 == 3 || C2 == 3 || C3 == 3 || C4 == 3)
-        {
-            col1.color = Naranja;
-        }
-        if (C1 == 4 || C2 == 4 || C3 == 4 || C4 == 4)
-        {
-            col1.color = Color.yellow;
-        }
-        if (C1 == 5 || C2 == 5 || C3 == 5 || C4 == 5)
-        {
-            col1.color = Color.green;
-        }
-        if (C1 == 6 || C2 == 6 || C3 == 6 || C4 == 6)
-        {
-            col1.color = Color.blue;
-        }
-        if (C1 == 7 || C2 == 7 || C3 == 7 || C4 == 7)
-        {
-            col1.color = Morado;
-        }
-        if (C1 == 8 || C2 == 8 || C3 == 8 || C4 == 8)
-        {
-            col1.color = Color.gray;
-        }
-        if (C1 == 9 || C2 == 9 || C3 == 9 || C4 == 9)
-        {
-            col1.color = Color.white;
-        }
-        #endregion
-
-        #region Numeros
-        if (N1 == 0 || N2 == 0 || N3 == 0 || N4 == 0 )
-        {
-            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S0;
-            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S0;
-            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S0;
-            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S0;
-            
-        }
-        if (N1 == 1 || N2 == 1 || N3 == 1 || N4 == 1)
-        {
-            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S1;
-            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S1;
-            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S1;
-            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S1;
-           
-        }
-        if (N1 == 2 || N2 == 2 || N3 == 2 || N4 == 2)
-        {
-            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S2;
-            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S2;
-            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S2;
-            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S2;
-            
-        }
-        if (N1 == 3 || N2 == 3 || N3 == 3 || N4 == 3)
-        {
-            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S3;
-            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S3;
-            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S3;
-            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S3;
-          
-
-        }
-        if (N1 == 4 || N2 == 4 || N3 == 4 || N4 == 4 )
-        {
-            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S4;
-            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S4;
-            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S4;
-            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S4;
-            
-        }
-        if (N1 == 5 || N2 == 5 || N3 == 5 || N4 == 5)
-        {
-            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S5;
-            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S5;
-            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S5;
-            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S5;
-            
-        }
-        if (N1 == 6 || N2 == 6 || N3 == 6 || N4 == 6)
-        {
-            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S6;
-            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S6;
-            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S6;
-            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S6;
-          
-        }
-        if (N1 == 7 || N2 == 7 || N3 == 7 || N4 == 7 )
-        {
-            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S7;
-            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S7;
-            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S7;
-            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S7;
-            
-        }
-        if (N1 == 8 || N2 == 8 || N3 == 8 || N4 == 8)
-        {
-            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S8;
-            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S8;
-            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S8;
-            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S8;
-          
-        }
-        if (N1 == 9 || N2 == 9 || N3 == 9 || N4 == 9)
-        {
-            D1.gameObject.GetComponent<SpriteRenderer>().sprite = S9;
-            D2.gameObject.GetComponent<SpriteRenderer>().sprite = S9;
-            D3.gameObject.GetComponent<SpriteRenderer>().sprite = S9;
-            D4.gameObject.GetComponent<SpriteRenderer>().sprite = S9;
-        }
-        #endregion
-
-
+        */
     }
 }
