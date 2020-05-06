0897fb4306d97e5f428e5c1f9cf309f6a430cf6e
TGmer <55259180+TGmer@users.noreply.github.com>
2020-05-06 18:04:25 -0500

    MenuObjetivos Ver 1

---
+++ TERRA/Assets/DadorObjetivos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DadorObjetivos : MonoBehaviour
+{
+    public Objetivos objetivo;
+    public PlayerController Jugador;
+    public GameObject MenuObj;
+    public Animator animador;
+    public bool MenuAbierto = false;
+    public Text NombreTxt;
+    public Text descripcionTxt;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && MenuAbierto == false)
+        {
+            animador.SetTrigger("Abrir");
+            MenuAbierto = true;
+            MenuObj.SetActive(true);
+            animador.SetTrigger("Stay");
+            Debug.Log("Menu abierto");
+            NombreTxt.text = objetivo.NombreMision;
+            descripcionTxt.text = objetivo.Descripcion;
+        }
+        else if(Input.GetKeyDown(KeyCode.Tab) && MenuAbierto == true)
+        {
+            animador.SetTrigger("Cerrar");
+            MenuAbierto = false;
+            animador.SetTrigger("StayClose");
+            Debug.Log("menu cerrado");
+          
+        }
+    }
+
+
+
+}
---
+++ TERRA/Assets/Objetivos.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Objetivos
+{
+    public bool isActive;
+
+    public string NombreMision;
+    public string Descripcion;
+
+}
