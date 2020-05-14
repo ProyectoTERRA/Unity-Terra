66c883bf61c24df53f853b97bd5d3a8620395672
TGmer <55259180+TGmer@users.noreply.github.com>
2020-05-13 20:35:50 -0500

    OU FUCKING SHET

---
+++ TERRA/Assets/Locker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Locker : MonoBehaviour
+{
+    public GameObject LockerAbierto;
+    void Start()
+    {
+        LockerAbierto = GetComponent<GameObject>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("ColisionLocker" +
+                "");
+            LockerAbierto.SetActive(true);
+
+        }
+
+    }
+}
