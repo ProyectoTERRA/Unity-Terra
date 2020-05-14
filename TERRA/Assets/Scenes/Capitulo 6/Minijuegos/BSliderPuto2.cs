20d290d2fe51c667bdbce7eb6ec3e93b3ce09bd9
RezLex <omegaphyton@gmail.com>
2020-05-14 12:23:55 -0500

    MinijuegoPuto2 Ready

---
+++ TERRA/Assets/Scenes/Capitulo 6/Minijuegos/BSliderPuto2.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BSliderPuto2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool Barempty, active, start;
+    public static int valF = 400;
+
+    public MiniPuto2 mini;
+
+    public Slider Bar;
+    [SerializeField] private GameObject  view;
+    void Start()
+    {
+        mini = GetComponent<MiniPuto2>();
+        Barempty = false;
+        Bar.value = valF;
+        StartCoroutine(push());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Barra:" + active);
+        if (start)
+        {
+            active = true;
+            start = false;
+            StartCoroutine(push());
+        }
+        if (!active)
+        {
+            Bar.enabled = false;
+        }
+        else
+        {
+            Bar.enabled = true;
+        }
+
+        if (Bar.value == 0 && active)
+        {
+            Barempty = true;
+            if (!MiniPuto2.c1 && !MiniPuto2.c2 && !MiniPuto2.c3)
+            {
+                MiniPuto2.countE = 0;
+            }
+            else if (MiniPuto2.c1 && !MiniPuto2.c2 && !MiniPuto2.c3)
+            {
+                MiniPuto2.countE = 1;
+
+            }
+            else if (MiniPuto2.c1 && MiniPuto2.c2 && !MiniPuto2.c3)
+            {
+                MiniPuto2.countE = 3;
+            }
+
+            StartCoroutine(mini.FAIL());
+            active = false;
+
+        }
+
+    }
+
+    public void Refill()
+    {
+        Bar.value = valF;
+    }
+
+    public IEnumerator push()
+    {
+
+        yield return new WaitForSeconds(.05f);
+
+        Debug.Log("DIM");
+        if (Bar.value > 0 && !Barempty)
+        {
+            Bar.value = Bar.value - 1;
+        }
+
+
+        if (!Barempty && active)
+        {
+            StartCoroutine(push());
+        }
+
+
+    }
+}
---
+++ TERRA/Assets/Scenes/Capitulo 6/Minijuegos/PruebaPuto2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PruebaPuto2 : MonoBehaviour
+{
+
+    [SerializeField] private GameObject View;
+    [SerializeField] private GameObject Cont;
+
+    private MiniPuto2 MIN;
+    bool put;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MIN = Cont.GetComponent<MiniPuto2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(MiniPuto2.enable);
+        if (MiniPuto2.enable)
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = Color.grey;
+        }
+        if (put)
+        {
+            StartCoroutine(MIN.Renabled());
+            View.SetActive(true);
+            put = false;
+        }
+    }
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && MiniPuto2.enable && !MiniPuto2.start1 && !MiniPuto2.active && Input.GetKeyDown(KeyCode.Return) && !MiniPuto2.c3)
+        {
+            put = true;
+        }
+    }
+}
