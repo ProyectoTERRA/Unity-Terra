9f5dde7f77161767820e2f93b72158863251428b
TGmer <55259180+TGmer@users.noreply.github.com>
2020-05-20 15:48:21 -0500

    EO

--- TERRA/Assets/Locker.cs
+++ TERRA/Assets/Locker.cs
@@ -4,21 +4,76 @@ using UnityEngine;
 
 public class Locker : MonoBehaviour
 {
-    public GameObject LockerAbierto;
+    public GameObject Lk1, Lk2, Lk3, Lk4, Lk5;
+    public bool L1, L2, L3, L4, L5;
     void Start()
     {
-        LockerAbierto = GetComponent<GameObject>();
+        Lk1 = GetComponent<GameObject>();
+        Lk2 = GetComponent<GameObject>();
+        Lk3 = GetComponent<GameObject>();
+        Lk4 = GetComponent<GameObject>();
+        Lk5 = GetComponent<GameObject>();
     }
 
-    private void OnTriggerEnter2D(Collider2D col)
+    private void Update()
     {
-
-        if (col.gameObject.tag == "Player")
+        if(L1 == true && Input.GetKey(KeyCode.E))
         {
-            Debug.Log("ColisionLocker");
-            LockerAbierto.SetActive(true);
-
+            Lk1.SetActive(true);
+            L1 = false;
         }
 
+        else if (L2 == true && Input.GetKey(KeyCode.E))
+        {
+            Lk2.SetActive(true);
+            L2 = false;
+        }
+
+        else if (L3 == true && Input.GetKey(KeyCode.E))
+        {
+            Lk3.SetActive(true);
+            L3 = false;
+        }
+
+        else if (L4 == true && Input.GetKey(KeyCode.E))
+        {
+            Lk4.SetActive(true);
+            L4 = false;
+        }
+
+        else if (L5 == true && Input.GetKey(KeyCode.E))
+        {
+            Lk5.SetActive(true);
+            L5 = false;
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Lock1")
+        {
+            L1 = true;
+        }
+
+        if (col.gameObject.tag == "Lock2")
+        {
+            L2 = true;
+        }
+
+        if (col.gameObject.tag == "Lock3")
+        {
+            L3 = true;
+        }
+
+        if (col.gameObject.tag == "Lock4")
+        {
+            L4 = true;
+        }
+
+        if (col.gameObject.tag == "Lock5")
+        {
+            L5 = true;
+        }
     }
 }
