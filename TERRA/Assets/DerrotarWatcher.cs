91bb7deac41135d7ee9e266e2e004965f8500054
TGmer <55259180+TGmer@users.noreply.github.com>
2020-05-21 12:30:43 -0500

    Eo

--- TERRA/Assets/DerrotarWatcher.cs
+++ TERRA/Assets/DerrotarWatcher.cs
@@ -5,25 +5,62 @@ using UnityEngine;
 public class DerrotarWatcher : MonoBehaviour
 {
     public GameObject btnDesact, btnAct;
-    Animator anim;
-    public bool Act, Desact;
+    public Animator anim;
+    public bool Act, Desact, PressA, PressD, Enable;
 
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject.tag == "BtnAct" && Act == true)
+        {
+            PressA = true;
+        }
+        if (collision.gameObject.tag == "BtnDes" && Desact == true)
+        {
+            PressD = true;
+        }
     }
 
     void Start()
     {
-        anim = GetComponent<Animator>();
+        Act = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(PressA == true && Input.GetKeyDown(KeyCode.E))
+        {
+            Activado();
+        }
+        else if (PressD == true && Input.GetKeyDown(KeyCode.E))
+        {
+            Desactivado();
+        }
 
     }
+
+    void Activado()
+    {
+        btnAct.SetActive(false);
+        anim.SetTrigger("Open");
+        anim.SetTrigger("StayO");
+        Desact = true;
+        Act = false;
+        PressA = false;
+        btnDesact.SetActive(true);
+    }
+    
+    void Desactivado()
+    {
+        btnDesact.SetActive(false);
+        anim.SetTrigger("Close");
+        anim.SetTrigger("StayC");
+        Desact = false;
+        Act = true;
+        PressD = false;
+        btnAct.SetActive(true);
+    }
+
 }
