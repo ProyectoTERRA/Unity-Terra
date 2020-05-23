31cb38fb528283b29eae923094b47e08231a878f
TGmer <55259180+TGmer@users.noreply.github.com>
2020-05-23 17:25:29 -0500

    CAP 6

--- TERRA/Assets/CameraFollow.cs
+++ TERRA/Assets/CameraFollow.cs
@@ -6,6 +6,7 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject follow;
     public Vector2 minCamPos, maxCamPos;
+    public Vector2 minCamSaved, maxCamSaved;
     public float smoothTime;
 
     private Vector2 velocity;
@@ -13,12 +14,15 @@ public class CameraFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        minCamSaved = minCamPos;
+        maxCamSaved = maxCamPos;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+    
+
         float posX = Mathf.SmoothDamp(transform.position.x, follow.transform.position.x,
             ref velocity.x, smoothTime);
         float posY = Mathf.SmoothDamp(transform.position.y, follow.transform.position.y,
@@ -28,5 +32,21 @@ public class CameraFollow : MonoBehaviour
             Mathf.Clamp(posX, minCamPos.x, maxCamPos.x),
             Mathf.Clamp(posY, minCamPos.y, maxCamPos.y),
             transform.position.z);
+
+        if (Input.GetKey(KeyCode.W)) 
+           {
+                
+            maxCamPos = maxCamPos + new Vector2(0, 3);
+            transform.position = new Vector3(
+            Mathf.Clamp(posX, minCamPos.x, maxCamPos.x),
+            Mathf.Clamp(posY + 3, minCamPos.y, maxCamPos.y),
+            transform.position.z);
+            smoothTime = 0.01f;
+        }
+        else
+        {
+            maxCamPos = maxCamSaved;
+            smoothTime = 0f;
+        }
     }
 }
