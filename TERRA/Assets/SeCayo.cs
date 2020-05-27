3ebd9b64d20f7d4b76e1e2bec0d9f0fff56bac20
RezLex <omegaphyton@gmail.com>
2020-05-27 16:29:20 -0500

    Merge branch 'master' of https://github.com/ProyectoTERRA/Unity-Terra

---
+++ TERRA/Assets/SeCayo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeCayo : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.SendMessage("TresCorazones");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
