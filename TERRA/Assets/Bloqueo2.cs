c7df91fa44189752506e21ff65ef91f9df0690fb
TGmer <55259180+TGmer@users.noreply.github.com>
2020-05-27 19:10:36 -0500

    NO LA VAYAS A CAGAR EMILIO

---
+++ TERRA/Assets/Bloqueo2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloqueo2 : MonoBehaviour
+{
+    private BoxCollider2D BC2D;
+    public GameObject Tiempo;
+
+    private void Start()
+    {
+        BC2D = GetComponent<BoxCollider2D>();
+    }
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            BC2D.isTrigger = true;
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        BC2D.isTrigger = false;
+        Tiempo.SetActive(true);
+    }
+}
