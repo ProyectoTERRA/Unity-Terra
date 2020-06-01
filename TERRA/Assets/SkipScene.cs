819c1ad76c40bda3270c05d80365ade0b92978cb
TGmer <55259180+TGmer@users.noreply.github.com>
2020-06-01 15:44:07 -0500

    aaaaa

---
+++ TERRA/Assets/SkipScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipScene : MonoBehaviour
+{
+
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace)) { 
+                
+            SceneManager.LoadScene("Lab2");
+        }    
+    }
+}
+    
