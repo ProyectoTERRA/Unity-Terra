8117cfda7085c1024aa51a95b5b22defc8bae0b2
RezLex <omegaphyton@gmail.com>
2020-06-02 17:46:33 -0500

    xcbv

--- TERRA/Assets/Scenes/Lobby/PlayerLobby.cs
+++ TERRA/Assets/Scenes/Lobby/PlayerLobby.cs
@@ -21,7 +21,7 @@ public class PlayerLobby : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameController.LobbyCAP = 4;
+        GameController.LobbyCAP = 2;
         Heart_Bar.Phearts = 6;
         Heart_Bar.life = 3;
         GameController.TypeLife = 1;
